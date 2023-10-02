95314f980b61a054e2213f95bf7c0c6d2a7300f5
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-02 17:43:55 +0100

    new changes

--- src/features/Features.jsx
+++ src/features/Features.jsx
@@ -5,24 +5,24 @@ function Features() {
       className="my-12 flex flex-col gap-[4rem] bg-white py-[5rem]"
     >
       <section className="flex flex-col items-center justify-center">
-        <h2 className="font-sora text-secondary text-[2.5rem] font-bold">
+        <h2 className="font-sora text-[2.5rem] font-bold text-secondary">
           Features
         </h2>
-        <p className="font-worksans text-grey text-xl font-normal">
+        <p className="font-worksans text-xl font-normal text-grey">
           Key Highlights of Our Extension
         </p>
       </section>
-      <section className="mx-auto flex w-screen flex-wrap items-center justify-center gap-[3.5rem] px-2 lg:w-[77.5rem] lg:flex-nowrap lg:px-12">
+      <section className="mx-auto flex w-screen flex-wrap items-center justify-center gap-[3.5rem] px-2 lg:flex-nowrap lg:px-12">
         <section className="flex w-[34.25rem] flex-col gap-12">
           <section className="flex items-start justify-start gap-4 self-stretch">
-            <span className="bg-tertiary flex w-16 items-center justify-center rounded-full p-2">
+            <span className="flex w-16 items-center justify-center rounded-full bg-tertiary p-2">
               <img src="./images/icon1.png" alt="icon pic" />
             </span>
             <section>
-              <h3 className="text-tertiary font-inter text-2xl font-semibold">
+              <h3 className="font-inter text-2xl font-semibold text-tertiary">
                 Simple Screen Recording
               </h3>
-              <p className="text-grey font-worksans text-lg font-normal">
+              <p className="font-worksans text-lg font-normal text-grey">
                 Effortless screen recording for everyone. Record with ease, no
                 tech expertise required.
               </p>
@@ -31,14 +31,14 @@ function Features() {
 
           {/* 2nd */}
           <section className="flex items-start justify-start gap-4 self-stretch">
-            <span className="bg-tertiary flex w-16 items-center justify-center rounded-full p-2">
+            <span className="flex w-16 items-center justify-center rounded-full bg-tertiary p-2">
               <img src="./images/icon2.png" alt="icon pic" />
             </span>
             <section>
-              <h3 className="text-tertiary font-inter text-2xl font-semibold">
+              <h3 className="font-inter text-2xl font-semibold text-tertiary">
                 Easy-to-Share URL
               </h3>
-              <p className="text-grey font-worksans text-lg font-normal">
+              <p className="font-worksans text-lg font-normal text-grey">
                 Share your recordings instantly with a single link. No
                 attachments, no downloads.
               </p>
@@ -47,14 +47,14 @@ function Features() {
 
           {/* 3rd  */}
           <section className="flex items-start justify-start gap-4 self-stretch">
-            <span className="bg-tertiary flex w-16 items-center justify-center rounded-full p-2">
+            <span className="flex w-16 items-center justify-center rounded-full bg-tertiary p-2">
               <img src="./images/icon3.png" alt="icon pic" />
             </span>
             <section>
-              <h3 className="text-tertiary font-inter text-2xl font-semibold">
+              <h3 className="font-inter text-2xl font-semibold text-tertiary">
                 Revisit Recordings
               </h3>
-              <p className="text-grey font-worksans text-lg font-normal">
+              <p className="font-worksans text-lg font-normal text-grey">
                 Access and review your past content effortlessly. Your
                 recordings, always at your fingertips.
               </p>
--- src/features/Hero.jsx
+++ src/features/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section className="flex flex-wrap justify-center gap-20 bg-white px-4 py-16 sm:gap-24 sm:py-32 lg:px-32 xl:flex-nowrap">
       <section className="flex flex-col items-start justify-center">
-        <h1 className="mb-[1.25rem] font-sora text-[3rem] font-bold leading-[100%] text-secondary sm:text-[4rem]">
+        <h1 className="mb-[1.25rem] font-sora text-[2.4rem] font-bold leading-[100%] text-secondary sm:text-[4rem]">
           Show Them <br /> Don’t Just Tell
         </h1>
         <p className="mb-12 font-inter text-lg font-normal text-[#000]/[.75] sm:w-[34.25rem]">
@@ -20,34 +20,36 @@ function Hero() {
         <section className="flex gap-4">
           <section className="flex flex-col gap-4">
             <img
-              className="z-10 w-[20.5rem] rounded-lg xl:w-[25rem] 2xl:w-[30rem]"
+              className="z-10 rounded-lg xl:w-[45rem] 2xl:w-[30rem]"
               src="./images/image1.png"
               alt="image"
             />
             <img
-              className="z-10 w-[20.5rem] rounded-lg xl:w-[25rem] 2xl:w-[30rem]"
+              className="z-10 rounded-lg xl:w-[45rem] 2xl:w-[30rem]"
               src="./images/image2.png"
               alt="image"
             />
           </section>
           <section className="flex">
             <img
-              className="z-10 w-[20rem] rounded-lg xl:w-[25rem] 2xl:w-[22.5rem]"
+              className="z-10 rounded-lg xl:w-[45rem] 2xl:w-[29rem]"
               src="./images/image3.png"
               alt="image"
             />
           </section>
         </section>
         <img
-          className="absolute -right-2 -top-8 w-[10rem] sm:-right-[1.9rem] sm:-top-[3.5rem] sm:w-[20rem] lg:-right-[1.8rem] lg:-top-[3.9rem] lg:w-[22rem] xl:-right-[.5rem] xl:top-[.8rem] xl:w-[10rem] 2xl:-right-[2.2rem] 2xl:-top-[3.9rem] 2xl:w-[22rem]"
+          className="absolute -right-2 -top-7 w-[10rem] sm:-right-[1.9rem] sm:-top-[3.5rem] sm:w-[20rem]  xl:-right-[1.4rem] xl:-top-[2.8rem] xl:w-[16rem] 2xl:-right-[2.7rem] 2xl:-top-[4.4rem] 2xl:w-[25rem]"
           src="./images/svg1.png"
           alt="dots"
         />
+        {/* lg:-right-[2.2rem] lg:-top-[3.9rem] lg:w-[22rem] */}
         <img
-          className="absolute -bottom-10 -left-0 w-[10rem] sm:-bottom-[5.2rem] sm:-left-[1.5rem] sm:w-[20rem] lg:-bottom-[.3rem] lg:-left-[0.5rem] lg:w-[22rem] xl:w-[10rem] 2xl:-bottom-[5.9rem] 2xl:-left-[1.6rem] 2xl:w-[22rem]"
+          className="absolute -bottom-10 -left-1 w-[10rem] sm:-bottom-[5.2rem] sm:-left-[1.5rem] sm:w-[20rem] xl:-bottom-[3.9rem] xl:-left-[1.1rem] xl:w-[16rem] 2xl:-bottom-[6.7rem] 2xl:-left-[2.1rem] 2xl:w-[25rem]"
           src="./images/svg2.png"
           alt="dots"
         />
+        {/* lg:-bottom-[5.9rem] lg:-left-[2rem] lg:w-[22rem] */}
       </section>
     </section>
   );
