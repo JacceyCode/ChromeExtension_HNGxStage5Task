7b9a8eae8caa16383c3689b0618b45a3fbd3a6d9
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-22 11:52:36 +0100

    added scrollToTup

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 function AppLayout() {
   return (
     <main className="bg-background">
+      <ScrollToTop />
       <Outlet />
     </main>
   );
